0f480f9562af1cedfb00fe614934d01d073ce840
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2025-05-26 11:05:12 +0530

    add operator code

--- DAY1/program1/HelloWorld.java
+++ DAY1/program1/HelloWorld.java
@@ -1,16 +1,16 @@
-// HelloWorld in java
+package program1;
 
 public class HelloWorld {
     HelloWorld() {
         System.out.println("Hello, World!");
     }
 
-    HelloWorld(String name) {   
+    HelloWorld(String name) {
         System.out.println("Hello, " + name + "!");
     }
 
     public static void main(String[] args) {
-        HelloWorld helloWorld = new HelloWorld();
-        HelloWorld helloWorld2 = new HelloWorld("Gaurav");
+        HelloWorld helloworld = new HelloWorld();
+        HelloWorld helloworld2 = new HelloWorld("Gaurav");
     }
 }
